f86f6f0b153c41d975c3dfd21cffe62ba40cb384
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-10-15 15:34:45 +0300

    add structure

---
+++ SAVE.ME/cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('cypress');
+const webpackConfig = require('./config/webpack.cypress.config');
+
+module.exports = defineConfig({
+    component: {
+        devServer: {
+            framework: 'react',
+            bundler: 'webpack',
+            webpackConfig, // webpackConfig: webpackConfig,
+        },
+        specPattern: ['src/**/*.cy.{js,jsx}'],
+        setupNodeEvents(on, config) {
+            // component testing node events setup code
+            // https://docs.cypress.io/guides/tooling/code-coverage
+            require('@cypress/code-coverage/task')(on, config);
+
+            on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+
+            return config;
+        },
+    },
+    
+    e2e: {
+        setupNodeEvents() {
+            // on, config не використовуємо ці зміні в цій функції тому виникає помилка eslint
+            // implement node event listeners here
+        },
+    },
+});
