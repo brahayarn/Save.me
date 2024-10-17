fc0ab312f55a9b18fec91affdb934341fd081fc4
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-10-17 23:13:20 +0300

    auth

---
+++ SAVE.ME/src/firebase/services/authService.js
@@ -0,0 +1,29 @@
+import { auth } from "../firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { validatePassword } from "./passwordValidation";
+
+export const signUp = async (email, password) => {
+  const passwordError = validatePassword(password);
+  if (passwordError) {
+    throw new Error(passwordError);
+  }
+  
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const logIn = async (email, password) => {
+  const passwordError = validatePassword(password);
+  if (passwordError) {
+    throw new Error(passwordError);
+  }
+  
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logOut = async () => {
+  return await signOut(auth);
+};
