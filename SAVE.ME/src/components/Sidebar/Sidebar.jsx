9214058c817a1d4ca87e808ce1161f47482dd551
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-10-26 19:45:55 +0300

    add routes

--- SAVE.ME/src/components/Sidebar/Sidebar.jsx
+++ SAVE.ME/src/components/Sidebar/Sidebar.jsx
@@ -1,51 +1,51 @@
+import burgerIcon from '@assets/icons/nonactive/burgerlist.svg';
+import groupsIcon from '@assets/icons/nonactive/groups.svg';
+import lightIcon from '@assets/icons/nonactive/lightmode.svg';
+import chatIcon from '@assets/icons/nonactive/messages.svg';
+import settingsIcon from '@assets/icons/nonactive/setting.svg';
+import tagsIcon from '@assets/icons/nonactive/tags.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import burgerIcon from "@assets/icons/nonactive/burgerlist.svg";
-import groupsIcon from "@assets/icons/nonactive/groups.svg"; 
-import lightIcon from "@assets/icons/nonactive/lightmode.svg";
-import chatIcon from "@assets/icons/nonactive/messages.svg";
-import settingsIcon from "@assets/icons/nonactive/setting.svg"; 
-import tagsIcon from "@assets/icons/nonactive/tags.svg"; 
-import React from "react";
-
-import styles from "./Sidebar.css";
+import styles from './Sidebar.css';
 
 const Sidebar = () => {
-  return (
-    <aside className={styles.chatSidebar}>
-      <ul className={styles.chatSidebarMenu}>
-      <li>
-          <a data-title="Burgerlist">
-            <img src={burgerIcon} alt="Burger Icon" className={styles.logoImage} />
-          </a>
-        </li>
-        <li className={styles.active}>
-          <a data-title="Chats">
-            <img src={chatIcon} alt="Chats Icon" className={styles.logoImage} />
-          </a>
-        </li>
-        <li>
-          <a data-title="Tags">
-            <img src={tagsIcon} alt="Tags Icon" className={styles.logoImage} />
-          </a>
-        </li>
-        <li>
-          <a data-title="Groups">
-            <img src={groupsIcon} alt="Groups Icon" className={styles.logoImage} />
-          </a>
-        </li>
-        <li>
-          <a data-title="Light">
-            <img src={lightIcon} alt="Light Icon" className={styles.logoImage} />
-          </a>
-        </li>
-        <li>
-          <a data-title="Settings">
-            <img src={settingsIcon} alt="Settings Icon" className={styles.logoImage} />
-          </a>
-        </li>
-      </ul>
-    </aside>
-  );
+    return (
+        <aside className={styles.chatSidebar}>
+            <ul className={styles.chatSidebarMenu}>
+                <li>
+                    <Link data-title="Burgerlist">
+                        <img src={burgerIcon} alt="Burger Icon" className={styles.logoImage} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/chat" data-title="Chats">
+                        <img src={chatIcon} alt="Chats Icon" className={styles.logoImage} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/tags" data-title="Tags">
+                        <img src={tagsIcon} alt="Tags Icon" className={styles.logoImage} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/groups" data-title="Groups">
+                        <img src={groupsIcon} alt="Groups Icon" className={styles.logoImage} />
+                    </Link>
+                </li>
+                <li>
+                    <Link data-title="Light">
+                        <img src={lightIcon} alt="Light Icon" className={styles.logoImage} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/settings" data-title="Settings">
+                        <img src={settingsIcon} alt="Settings Icon" className={styles.logoImage} />
+                    </Link>
+                </li>
+            </ul>
+        </aside>
+    );
 };
 
 export default Sidebar;
--- SAVE.ME/src/pages/Auth/Auth.jsx
+++ SAVE.ME/src/pages/Auth/Auth.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import backimg from '../../assets/images/backimg.svg';
 import textImage from '../../assets/images/textpg.png';
 import styles from './Auth.css';
 
 const Auth = () => {
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        navigate('/profile');
+    };
     return (
-        <div className={styles.main} style={{ backgroundImage: `url(${backimg})` }}>
+        <div className={styles.maincont} style={{ backgroundImage: `url(${backimg})` }}>
             <div className={styles.authform}>
                 <form>
                     <div className={styles.top__container}>
                         <div>
-                            <h1>Будь в потоці</h1>
-                            <input type="email" placeholder="Email..." />
+                            <h1 className={styles.c}>Будь в потоці</h1>
+                            <input type="email" placeholder="Email..." className={styles.inputc} />
                         </div>
                         <div>
-                            <input type="password" placeholder="Password..." />
+                            <input type="password" placeholder="Password..." className={styles.inputc} />
                         </div>
-                        <button className={styles.button}>Зареєструватися</button>
+                        <button className={styles.buttc}>Зареєструватися</button>
                     </div>
 
                     <div className={styles.bottom__container}>
-                        <h2>Уже зареэстровані?</h2>
-                        <button className={styles.button}>Ввійти</button>
+                        <h2 className={styles.c2}>Уже зареэстровані?</h2>
+                        <button onClick={handleLogin} className={styles.buttc}>
+                            Ввійти
+                        </button>
                     </div>
                 </form>
             </div>
